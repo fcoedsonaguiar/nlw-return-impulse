1cdcd8f8eb71235ebe6c4b0d63ff128cead4b62b
Fco Edson Aguiar Martins <fcoedsonaguiar@gmail.com>
2022-05-10 09:21:33 -0300

    remove server into the web folder

--- web/server/prisma/migrations/20220505144452_create_feedback_table/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Feedback" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "type" TEXT NOT NULL,
-    "feedbackContent" TEXT NOT NULL,
-    "screenshot" TEXT
-);
--- web/server/src/repositories/feedbacks-repository.ts
+++
@@ -1,9 +0,0 @@
-export interface FeedbackCreateData {
-    type: string
-    feedbackContent: string
-    screenshot?: string
-}
-
-export interface FeedbackRepository {
-    create: (data: FeedbackCreateData) => Promise<void>;
-}
